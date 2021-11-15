cd141525f2491b1d2ab01d6a668e4acff5bf71c1
WELLINGTON <wellington_1992@hotmail.com>
2021-11-15 17:47:20 -0300

    projeto inicial

---
+++ Cursomvc/Migrations/20211115134210_TabelaProduto.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cursomvc.Migrations
+{
+    public partial class TabelaProduto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Produtos_Categorias_CategoriaId",
+                table: "Produtos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoriaId",
+                table: "Produtos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Produtos_Categorias_CategoriaId",
+                table: "Produtos",
+                column: "CategoriaId",
+                principalTable: "Categorias",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Produtos_Categorias_CategoriaId",
+                table: "Produtos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoriaId",
+                table: "Produtos",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Produtos_Categorias_CategoriaId",
+                table: "Produtos",
+                column: "CategoriaId",
+                principalTable: "Categorias",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+## Curso DIO 
+
+##### Desenvolvimento de aplicações com .NET
+
+Contexto do curso:
+
+#### Conheça o Entity Framework e crie aplicações ASP.NET
+1.  O que é Entity Framework
+2. 	Introdução ao ASP .NET MVC
+3.	Criando a primeira aplicação ASP.NET
+3.	Desenvolvendo a aplicação
+3.	Organizando a descrição
+3.	Aplicando novas categorias no projeto
+
